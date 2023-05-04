ec1797c176e30e2cbbc8f5ceed0e707d6065d433
choda20 <itaykadchoda@gmail.com>
2023-05-04 22:10:37 +0300

    lesson4 added

---
+++ pythonBasicCourse/small-assigments/lesson3.py
@@ -0,0 +1,31 @@
+def ex321():  # 3.2.1
+    print(
+        "\"Shuffle, Shuffle, Shuffle\", say it together!\nChange colors and directions,\nDon't back down and stop the "
+        "player!\n\t\tDo you want to play Taki?\n\t\tPress y\\n")
+
+
+def ex333():  # 3.3.3
+    encrypted_message = "!XgXnXiXcXiXlXsX XnXoXhXtXyXpX XgXnXiXnXrXaXeXlX XmXaX XI"
+    print(encrypted_message[-1::-2])
+
+
+def ex342():  # 3.4.2
+    user_input = input("Enter a string: ")
+    print(user_input[0] + user_input[1:].replace(user_input[0], "e"))
+
+
+def ex343():
+    user_input = input("Enter a string: ")
+    half_length = len(user_input) // 2
+    print(user_input[0:half_length].lower() + user_input[half_length:].upper())
+
+
+if __name__ == '__main__':
+    print("ex 3.2.1: ")
+    ex321()
+    print("\nex 3.3.3: ")
+    ex333()
+    print("\nex 3.4.2: ")
+    ex342()
+    print("\nex 3.4.3: ")
+    ex343()
---
+++ pythonBasicCourse/small-assigments/lesson4.py
@@ -0,0 +1,35 @@
+import datetime
+
+
+def ex422():
+    user_input = input("Enter a string: ")
+    if user_input[-1::-1].lower().replace(" ", "") == user_input[0::].lower().replace(" ", ""):
+        print("OK")
+    else:
+        print("NOT")
+
+
+def ex423():
+    user_input = input("Insert the temperature you would like to convert: ").lower()  # assuming the input is valid
+    temperature = float(user_input[0:len(user_input) - 1])
+    if user_input.endswith("c"):
+        print(str(((temperature * 9) + (32 * 5)) / 5) + "F")
+    else:
+        print(str(((temperature * 5) - 160) / 9) + "C")
+
+
+def ex424():
+    user_input = input("Enter a date: ")
+    date_list = user_input.split("/")
+    week_days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    week_num = datetime.date(int(date_list[2]), int(date_list[1]), int(date_list[0])).weekday()
+    print(week_days[week_num])
+
+
+if __name__ == '__main__':
+    print("ex 4.2.2: ")
+    ex422()
+    print("\nex 4.2.3: ")
+    ex423()
+    print("\nex 4.2.4: ")
+    ex424()
